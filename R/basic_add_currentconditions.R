6e9c8695cb82fdc44af60bf196281632ad6af298
Guy Lafleur <kevin.netherton@gov.bc.ca>
2024-10-04 11:26:31 -0700

    feat: r rha data population (#8)
    Co-authored-by: Jon Goetz <jon.goetz@gov.bc.ca>

---
+++ R/basic_add_currentconditions.R
@@ -0,0 +1,34 @@
+# install.packages("RPostgres")
+# install.packages("DBI")
+
+
+library(DBI)
+
+# setting up the database connection parameters
+db <- Sys.getenv("POSTGRES_DB", "postgres")
+db_host <- Sys.getenv("POSTGRES_HOST", "localhost")
+db_port <- Sys.getenv("POSTGRES_PORT", "5433")
+db_user <- Sys.getenv("POSTGRES_USER", "postgres")
+db_pass <- Sys.getenv("POSTGRES_PASSWORD", "default")
+
+conn <- dbConnect(
+  RPostgres::Postgres(),
+  dbname = db,
+  host = db_host,
+  port = db_port,
+  user = db_user,
+  password = db_pass
+)
+
+print(conn)
+
+current <- read.csv("data/StationInformation.csv")
+
+# as a best practice you would be better off, using dbAppendTable so that if the table 
+# doesn't exist an error gets raised as the table should have been created by a migration
+# DBI::SQL("hydro.current_conditions"),
+dbWriteTable(conn, DBI::SQL("hydro.current_conditions"), value = current, overwrite = TRUE)
+
+dbListTables(conn)
+query <- sprintf("SELECT * FROM hydro.current_conditions")
+df <- dbGetQuery(conn, query)
---
+++ migrations/sql/V5.2__current_conditions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE hydro.current_conditions (
+	"Station.ID" text NULL,
+	"Station.Name" text NULL,
+	"LATITUDE" float8 NULL,
+	"LONGITUDE" float8 NULL,
+	"Current.Reading." text NULL,
+	"Return.Period" text NULL,
+	"RTP_COLOR" int4 NULL,
+	"PCT.of.Mean.Ann.Disch." text NULL,
+	"PCT_CL" int4 NULL,
+	"WSC.Real.Time.Data" text NULL,
+	"Updated.at" text NULL
+);
